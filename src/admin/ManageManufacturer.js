96b8e42187eba9a71a829c270d8fa41d0dd5faff
ashish-keasofttech <aashishr01@keasofttech.com>
2022-03-01 16:41:19 +0530

    delete message use /done gaurav

--- src/admin/ManageManufacturer.js
+++ src/admin/ManageManufacturer.js
@@ -3,8 +3,17 @@ import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
 import { deleteManufacturer, getManufacturers, deleteManufacturer1 } from "./apiAdmin";
 import { Switch } from '@mui/material';
+import { Redirect } from 'react-router-dom';
 
 const ManageManufacturer = () => {
+    
+    const [values, setValues] = useState({
+         error: '',
+        redirectToProfile: false,
+        success: false
+    });
+    const { error, success, redirectToProfile } = values;
+    
     const [products, setProducts] = useState([]);
 
     const { user, token } = isAuthenticated();
@@ -13,7 +22,7 @@ const ManageManufacturer = () => {
 
     const loadProducts = () => {
         getManufacturers().then(data => {
-        console.log("gaurav", data);
+       // console.log("gaurav", data);
             if (data.error) {
                 console.log(data.error);
             } else {
@@ -28,6 +37,17 @@ const ManageManufacturer = () => {
                 console.log(data.error);
             } else {
                 loadProducts();
+                setValues({
+                    ...values,
+                    success:true,
+                    redirectToProfile: false
+                });
+                setTimeout(function(){
+                    setValues({
+                        ...values,
+                        redirectToProfile:true
+                    })
+                },1000)
             }
         });
     };
@@ -50,6 +70,20 @@ const ManageManufacturer = () => {
         });
     };
 
+    const deleteMessage = () => (
+        <div className="alert alert-danger" style={{ display: success ? '' : 'none' }}>
+           <a class="text-center" style={{color:'white'}}> Manufacturer Deleted </a> 
+        </div>  
+    );
+    const redirectUser = () => {
+        if (redirectToProfile) {
+            if (!error) {
+                return <Redirect to="/admin/manufacturers" />;
+            }
+        }
+    };
+
+
     useEffect(() => {
         loadProducts();
     }, []);
@@ -61,10 +95,14 @@ const ManageManufacturer = () => {
     }
 
     return (
+        
 
             <div className="row">
+                {deleteMessage()}
+                {redirectUser()}
                 <div className="col-12">
                     <table className="table">
+                    
                     <thead>
                             <tr id="TH">
                                 <th><input type="checkbox" id="checkboxTH"/></th>
@@ -86,7 +124,16 @@ const ManageManufacturer = () => {
                                 <td>{p.manufacturerName}</td>
                                 <td>{p.description}</td>
                                 <td>{getDate(p.createdAt)} </td>
-                                <td><Switch name="checkedA" inputProps={{ "aria-label": "secondary checkbox","size": "medium","color":"primary" }} color='primary'/></td>
+                                <td>
+                                {p.status == 1 
+                                    ?(
+                                    <>
+                                      <Switch name="checkedA" checked inputProps={{ "aria-label": "secondary checkbox","size": "medium","color":"Primary" }} color='primary'/>
+                                    </>
+                                    ):
+                                     <Switch name="checkedA"  inputProps={{ "aria-label": "secondary checkbox","size": "medium","color":"Primary" }} color='primary'/>
+                                    }
+                                </td>
                                 <td>
                                     <Link to={`/admin/manufacturer/update/${p._id}`}><button className='btn btn-outline btn-info m-5' aria-label='Edit' title="Add Manufacturer"><i className='fa fa-pencil font-15'></i></button></Link>
                                     <button className='btn btn-outline btn-danger' aria-label='Delete' onClick={() => destroy(p._id)} title="Delet"><i className='fa fa-trash-o font-15'></i></button>
