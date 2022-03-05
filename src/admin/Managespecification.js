291a733a32775e1d7f3639cdefc2b7949cfa3ef8
yogeshkorade <yogesh.korade5@gmail.com>
2022-03-05 09:41:52 +0530

    Yogesh : Specification datatable changes

--- src/admin/Managespecification.js
+++ src/admin/Managespecification.js
@@ -56,43 +56,65 @@ const Managespecification = () => {
         text: 'action'
       }];
 
-    return (
-
-            <div className="row">
-                <h4 className="box-title">Total List of Specification {products.length}</h4><hr></hr>
-                <div className="col-12">
-                   {products != "" ? <DataTableComponent title="Test" tableHeading={columns} tableList={products}/> : null}
-                    {/*<table className="table">
-                    <thead>
-                            <tr>
-                                <th>Specification Name</th>
-                                <th>Date</th>
-                                <th>Status</th>
-                                <th>Action</th>
-                            </tr>
-                    </thead>
-                    <tbody>
-                        {products.map((p, i) => (
-                       <tr  key={i}>
-                            <td>{p.manufacturerName}</td>
-                            <td>{getDate(p.createdAt)} </td>
-                            <td><Switch name="checkedA" inputProps={{ "aria-label": "secondary checkbox","size": "medium","color":"primary" }} color='primary'/></td>
-                            
-                            <td>
-                                <Link to={`/admin/Updatespecification/update/${p._id}`}><button className='btn btn-outline btn-info m-5' aria-label='Edit'><i className='fa fa-pencil font-15'></i></button></Link>
-                                <button className='btn btn-outline btn-danger' aria-label='Delete' onClick={() => destroy(p._id)}><i className='fa fa-trash-o font-15'></i></button>
-                            </td>     
-                        
-                        </tr>
-                        
-                        ))}
-
-                    </tbody>
-                        </table>*/}
-                    
-                    <br />
-                </div>
+      const getButtons = (product) => {
+        return (
+            <div>
+                <Link to={`/admin/Updatespecification/update/${product._id}`}><button className='btn btn-outline btn-info m-5' aria-label='Edit'><i className='fa fa-pencil font-15'></i></button></Link>
+                <button className='btn btn-outline btn-danger' aria-label='Delete' onClick={() => destroy(product._id)}><i className='fa fa-trash-o font-15'></i></button>
             </div>
+        )
+      };
+
+      const getSwitch = (product) => {
+        return (
+            <Switch name="checkedA" inputProps={{ "aria-label": "secondary checkbox","size": "medium","color":"primary" }} color='primary'/>
+        )
+      };
+
+      const productsList = [];
+      products.forEach((item) => {
+        item['createdAt'] = getDate(item.createdAt);
+        item['status'] = getSwitch(item);
+        item['action'] = getButtons(item);
+        productsList.push(item);
+      });
+      
+      return (
+        <div className="row">
+            <h4 className="box-title">Total List of Specification {products.length}</h4><hr></hr>
+            <div className="col-12">
+                {productsList != "" ? <DataTableComponent title="Test" tableHeading={columns} tableList={productsList}/> : null}
+                {/*<table className="table">
+                <thead>
+                        <tr>
+                            <th>Specification Name</th>
+                            <th>Date</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                </thead>
+                <tbody>
+                    {products.map((p, i) => (
+                    <tr  key={i}>
+                        <td>{p.manufacturerName}</td>
+                        <td>{getDate(p.createdAt)} </td>
+                        <td><Switch name="checkedA" inputProps={{ "aria-label": "secondary checkbox","size": "medium","color":"primary" }} color='primary'/></td>
+                        
+                        <td>
+                            <Link to={`/admin/Updatespecification/update/${p._id}`}><button className='btn btn-outline btn-info m-5' aria-label='Edit'><i className='fa fa-pencil font-15'></i></button></Link>
+                            <button className='btn btn-outline btn-danger' aria-label='Delete' onClick={() => destroy(p._id)}><i className='fa fa-trash-o font-15'></i></button>
+                        </td>     
+                    
+                    </tr>
+                    
+                    ))}
+
+                </tbody>
+                    </table>*/}
+                
+                <br />
+            </div>
+        </div>
     );
 };
 
