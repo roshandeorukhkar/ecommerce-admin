d803fdfd2f5f60d77fd55a053871ebb19d9736a3
ashish-keasofttech <aashishr01@keasofttech.com>
2022-05-31 11:14:06 +0530

    CROS error

--- src/auth/User.js
+++ src/auth/User.js
@@ -5,7 +5,9 @@ export const adminsignin = admin => {
         method: 'POST',
         headers: {
             Accept: 'application/json',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
         },
         body: JSON.stringify(admin)
     })
